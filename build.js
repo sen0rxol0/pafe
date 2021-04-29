d722d41f3c193caee918d851619759690b566228
sen0rxol0 <sen0rxol0@baieflow.com>
2021-04-29 11:57:08 +0200

    Added build script for use in packaging

---
+++ build.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+// Copyright 2021 BAIELFOW. All rights reserved.
+// Please see the LICENCE file for use of this source code.
+
+const { exec } = require('child_process'); 
+
+const VERSION = require('./package').version;
+
+function execCmd(cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`ERROR in exec: ${err}`);
+        return;
+      }
+
+      console.log(`${stdout}`);
+      //console.log(`stderr: ${stderr}`);
+      if (stderr) reject(stderr);
+      resolve(stdout);
+    });
+  });
+}
+
+execCmd('npx gulp package').then(() => {
+  execCmd(`zip -rq pafe-macOS-v${VERSION}.zip ./build/Pafe-darwin-x64/Pafe.app && rm -r build`)
+  .then(() => {
+    console.log('build for macOS complete.');
+    process.exit(0);
+  });
+});
