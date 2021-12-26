bcb73505e29ab7342837174a92f21e46ba2da3a1
sen0rxol0 <sen0rxol0@baieflow.com>
2021-12-26 11:45:22 +0100

    Improved build script so yue can be downloaded for each target platform

--- build.js
+++ build.js
@@ -5,8 +5,9 @@
 
 const { exec } = require('child_process');
 const VERSION = require('./package.json').version;
-// TODO
-// download or build gui for each platform 
+const onExecCatch = (err) => console.error(`${err}\n`);
+const electronVersion = '16';
+const yueTagName = 'v0.10.1';
 
 function execCmd(cmd) {
   return new Promise((resolve, reject) => {
@@ -14,28 +15,47 @@ function execCmd(cmd) {
       if (err) {
         console.error(`${err}\n`);
       }
-      console.log(`${stdout}\n`);
+      // console.log(`${stdout}\n`);
       if (stderr) reject(stderr);
       resolve(stdout);
     });
   });
 }
 
-const onExecCatch = (err) => console.error(`${err}\n`);
+function downloadYue(platform) {
+  const yueDownloadUrl = `https://github.com/yue/yue/releases/download/${yueTagName}/node_yue_electron_${electronVersion}_${yueTagName}_${platform}.zip`;
+  const filename = `gui_electron_${platform}.zip`;
+  return execCmd(`wget -q -O ${filename} ${yueDownloadUrl}; unzip -o ${filename} -x LICENSE; rm ${filename}`);
+}
 
-execCmd('npx gulp pack').then(() => {
-  execCmd(`cd build/Pafe-darwin-x64 && zip -rq -9 pafe-v${VERSION}-macOS.zip ./Pafe.app`)
-  .then(() => {
-    console.log('build for mac platform is complete.\n');
+function packDarwin() {
+  return downloadYue('mac_x64').then(() => {
+    return execCmd('npx gulp pack').then(() => {
+      return execCmd(`cd build/Pafe-darwin-x64 && zip -rq -9 pafe-v${VERSION}-macOS.zip ./Pafe.app`);
+    }).catch(onExecCatch);
   }).catch(onExecCatch);
-}).catch(onExecCatch);
+}
 
-execCmd('npx gulp pack:linux').then(() => {
-  execCmd(`cd build/Pafe-linux-x64/ && zip -rq -9 pafe-v${VERSION}-linux.zip ./`)
-  .then(() => {
+function packLinux() {
+  return downloadYue('linux_x64').then(() => {
+    return execCmd('npx gulp pack:linux').then(() => {
+      return execCmd(`cd build/Pafe-linux-x64/ && zip -rq -9 pafe-v${VERSION}-linux.zip ./`);
+    }).catch(onExecCatch);
+  }).catch(onExecCatch);
+}
+
+packDarwin().then(() => {
+  console.log('build for mac platform is complete.\n');
+  packLinux().then(() => {
     console.log('build for linux platform is complete.\n');
+    downloadYue('mac_x64');
   }).catch(onExecCatch);
 }).catch(onExecCatch);
+
+// execCmd('curl https://api.github.com/repos/yue/yue/releases/latest > yue_releases.json').then(() => {
+//   // const tagName = JSON.parse(res).tag_name;
+//   // console.log(tagName);
+// }).catch(onExecCatch);
 
 // execCmd('npx gulp pack:win').then(() => {
 //   execCmd(`zip -rq pafe-v${VERSION}-win.zip ./build/Pafe-darwin-x64/Pafe.app && rm -r build`)
